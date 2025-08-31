c751d600bf057db20b65e3bca0520933c664ce25
yebis0942 <yebis0942@gmail.com>
2025-08-31 12:34:50 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yebis0942/kyotogo-2025-08-31-lt
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		log.Fatalf("Environment variable PORT is not set")
+	}
+
+	mux := NewServer()
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+	log.Printf("Server listening on :%s", port)
+}
+
+func NewServer() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", helloHandler)
+	mux.HandleFunc("GET /health", healthHandler)
+	return mux
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.WriteString(w, "Hello, world!"); err != nil {
+		log.Printf("Error writing response for '/': %v", err)
+	}
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if _, err := io.WriteString(w, `{"status": "ok"}`); err != nil {
+		log.Printf("Error writing response for '/health': %v", err)
+	}
+}
